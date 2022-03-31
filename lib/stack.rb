bffc9da38f15de28583d959bb30f16460ceb07fb
Matt Quinn <mjq0533@gmail.com>
2022-03-30 19:04:24 -0500

    commit tree de793f42840de3a4165e2c51ba1fedc0ff03ae45

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,40 @@
-# your code here
+class Stack
+    def initialize(limit = nil)
+      @stack = []
+      @limit = limit
+    end
+  
+    def push(item)
+      raise 'Stack Overflow' if full?
+  
+      @stack.push(item)
+    end
+  
+    def pop
+      @stack.pop
+    end
+  
+    def peek
+      @stack.last
+    end
+  
+    def size
+      @stack.length
+    end
+  
+    def empty?
+      @stack.empty?
+    end
+  
+    def full?
+      @limit && @stack.size == @limit
+    end
+  
+    def search(target)
+      @stack.each_with_index do |item, idx|
+        return size - idx - 1 if item == target
+      end
+  
+      -1
+    end
+  end
